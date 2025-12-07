4cef224b80ae7a16d0f69b51b92d8db51b7e75a0
EngineX-GB <uk.engine.x@gmail.com>
2025-12-07 19:11:32 +0000

    Added the order table

--- db-schema-patch.sql
+++ db-schema-patch.sql
@@ -9,3 +9,23 @@ drop view vw_todays_watchlist;
 create view vw_todays_watchlist as 
 select user_id, username, nationality, telephone, location, rate_1_hour from tbl_client 
 where user_id in (select user_id from tbl_client_watchlist) and date(refresh_time) = date(now());
+
+
+create table if not exists tbl_order (
+	oid integer not null auto_increment,
+	user_id varchar(10),
+	location varchar(200),
+    region varchar(100),
+	date_of_event date,
+	time_of_event time,
+	creation_date datetime,
+	modification_date datetime,
+	duration integer,
+	rate integer,
+	deductions integer,
+	surplus integer,
+	price integer, 			-- the final price
+	status varchar(20),
+	notes text,
+	primary key (oid)
+);
